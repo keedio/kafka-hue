d2e98ab8390180e89d6da9f97be4d1039b9b5107
danieltardon <dtardon@keedio.com>
2015-02-27 13:35:28 +0100

    License Headers and update url for Setup.py

--- kafka/setup.py
+++ kafka/setup.py
@@ -1,12 +1,12 @@
-# Licensed to Cloudera, Inc. under one
+# Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
-# regarding copyright ownership.  Cloudera, Inc. licenses this file
+# regarding copyright ownership.  The ASF licenses this file
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http:# www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -28,7 +28,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 setup(
   name = "kafka",
   version = "2.0",
-  url = 'www.redoop.org',
+  url = 'www.keedio.com',
   description = 'Kafka HUE integration',
   author = 'dtardon',
   packages = find_packages('src'),
